c4b25e46234413352e6ab0172e952bd3f2704475
Denarius Motes <135556018+Motesart27@users.noreply.github.com>
2026-04-23 13:07:28 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useAuth } from '../context/AuthContext'
+import { useAuth } from './context/AuthContext'
 import { useNavigate } from 'react-router-dom'
 import api from '../services/api'
 
